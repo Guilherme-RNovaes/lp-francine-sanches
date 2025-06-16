6af57e4c8340cf9701c3449a635709d1162e73f8
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-06-16 15:16:00 -0300

    chore: add initial options

---
+++ options/navigation.ts
@@ -0,0 +1,30 @@
+export type navigation = {
+  name: string;
+  href: string;
+  id: string;
+}
+
+const NavigationProps: navigation[] = [
+  {
+    name: 'Home',
+    href: '/#home',
+    id: 'home',
+  },
+  {
+    name: 'Serviços',
+    href: '/#services',
+    id: 'services',
+  },
+  {
+    name: 'Sobre',
+    href: '/#about',
+    id: 'about',
+  },
+  {
+    name: 'Contato',
+    href: '/#contact',
+    id: 'contact',
+  },
+]
+
+export default NavigationProps;
---
+++ options/user.ts
@@ -0,0 +1,24 @@
+export interface User {
+  name: string
+  lastName: string
+  profession: string;
+  heroText: string;
+  about: string;
+  contactText: string;
+  heroImageUrl: string;
+  aboutImageUrl: string;
+}
+
+const userProps: User =
+{
+  name: 'Francine Sanches',
+  lastName: 'Novaes',
+  profession: 'Psicóloga Clínica | Terapia Cognitivo-Comportamental (TCC)',
+  heroText: '"Você não precisa viver presa(o) à ansiedade, autocobrança e pensamentos negativos. A Terapia Cognitivo-Comportamental (TCC) oferece ferramentas práticas para lidar com emoções difíceis, fortalecer sua autoestima e construir uma vida mais leve, com equilíbrio e propósito."',
+  about: 'Desenvolvedor autônomo, especializado em criar soluções digitais como Landing Pages, E-commerces e Páginas Institucionais, unindo funcionalidade, design e alta performance para potencializar sua presença online.',
+  contactText: 'Estou aqui para te ouvir. Caso tenha dúvidas, queira agendar uma sessão ou apenas saber mais sobre o meu trabalho, sinta-se à vontade para entrar em contato.',
+  heroImageUrl: '/images/francine-sanches-hero-photo.png',
+  aboutImageUrl: '/images/francine-sanches-about-photo.jpg',
+}
+
+export default userProps;
