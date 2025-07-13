48720b49d50d48a4eb4eac26baae0fe381163f12
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2025-07-13 11:18:56 -0300

    feat: add services header animation

--- app/(home)/_components/services/index.tsx
+++ app/(home)/_components/services/index.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import ServicesProps from "@/options/services";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import Image from "next/image";
 import BookingButton from "@/app/_components/cal-button";
+import { useGSAP } from "@gsap/react";
+import SplitType from "split-type";
 
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Services() {
-  const containerRef = useRef<HTMLDivElement>(null)
+  const servicesContainerRef = useRef<HTMLDivElement>(null)
   const imageRefs = useRef<(HTMLImageElement | null)[]>([])
 
   const handleMouseEnter = (index: number) => {
@@ -30,15 +32,37 @@ export default function Services() {
     gsap.to(el, { autoAlpha: 0, scaleY: 0, duration: 0.2, ease: "power2.inOut" })
   }
 
-  useEffect(() => {
-    if (!containerRef.current) return
+  useGSAP(() => {
+
+
+    if (!servicesContainerRef.current) return
+
+    const splitChars = new SplitType('.reveal-services-header', {
+      types: 'chars',
+    })
+    gsap.fromTo(
+      splitChars.chars,
+      { y: "115%" },
+      {
+        y: "0%",
+        ease: "power4.out",
+        duration: 0.7,
+        stagger: 0.03,
+        scrollTrigger: {
+          trigger: servicesContainerRef.current,
+          start: 'top 80%',
+          markers: true,
+        }
+      }
+    )
 
     gsap.utils.toArray('.highlight').forEach((el, index) => {
       gsap.fromTo(
         el as Element,
-        { backgroundSize: '0% 100%' },
+        { backgroundSize: '0% 100%', borderRadius: "4px" },
         {
           backgroundSize: '100% 100%',
+          borderRadius: "4px",
           duration: 1,
           ease: 'power2.out',
           delay: index * 0.4,
@@ -49,11 +73,18 @@ export default function Services() {
         }
       )
     })
+    return () => {
+      splitChars.revert() // limpa os spans injetados pelo SplitType
+    }
   }, [])
   return (
     <section id="services" className="min-h-screen w-screen flex flex-col items-center text-foreground justify-between bg-background-secondary relative pt-20 md:pb-20 px-10 rounded-t-4xl">
-      <h2 className="font-sub-title text-[9vw] md:text-[10vw] xl:text-[11.2vw] pb-24">Como posso te ajudar</h2>
-      <div ref={containerRef} className="grid gap-20 w-full">
+      <h2 className="font-sub-title text-[9vw] md:text-[10vw] xl:text-[11.2vw] pb-24 overflow-hidden">
+        <span className="reveal-services-header translate-y-[100%] pr-1">
+          Como posso te ajudar
+        </span>
+      </h2>
+      <div ref={servicesContainerRef} className="grid gap-20 w-full">
         {ServicesProps.map((item, i) => (
           <div
             key={i}
@@ -67,6 +98,7 @@ export default function Services() {
             <Image
               src={item.image}
               alt={item.title}
+              ref={el => { imageRefs.current[i] = el }}
               width={250}
               height={250}
               className="hidden md:block absolute left-40 top-1/2 -translate-y-1/2 z-10 rounded-xl pointer-events-none opacity-0"
@@ -74,7 +106,7 @@ export default function Services() {
             <div>
               <h2 className="text-2xl md:text-4xl xl:text-7xl pb-10 font-semibold font-sub-title border-b">{item.title}</h2>
               <p
-                className="text-sm md:text-xl xl:text-2xl pt-10 text-muted-foreground mt-1 max-w-2xl"
+                className="text-sm md:text-xl xl:text-2xl pt-10 text-foreground mt-1 max-w-2xl"
                 dangerouslySetInnerHTML={{ __html: item.description }}
               />
             </div>
